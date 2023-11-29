b02d48614d3368e75701a5aac613dc1cc517e237
Phạm Trâm Anh <93437075+tramanhpham@users.noreply.github.com>
2023-11-29 09:03:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import pandas as pd
 
 st.title('Data visualization')
 
+st.header('Upload data file')
 data_file = st.file_uploader('Choose a csv file', type=(['.csv']))
 
 if data_file is not None:
   df = pd.read_csv(data_file)
 
+  st.header('Show data')
   st.dataframe(df)
-                                                         
+
+  st.header('Descriptive statistics')
+  st.table(df.describe())
