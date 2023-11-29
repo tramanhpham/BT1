89eaa1389009abcb725ad4199fc164d94e4d35af
Phạm Trâm Anh <93437075+tramanhpham@users.noreply.github.com>
2023-11-29 08:55:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
 
+st.title('Data visualization')
+
+data_file = st.file_uploader('Choose a csv file', type=(['.csv]))
+
+if data_file is not None:
+  df = pd.read_csv(data_file)
+
+  st.dataframe(df)
+                                                         
